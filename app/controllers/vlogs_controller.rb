d69fec43659e16f5a524a227b7736cfb486a6c07
Dave J <david@dvj.me.uk>
2019-12-16 22:12:44 +0000

    Add multiple file associated to vlog.rb
    This commit adds the multiple `files` association to our Vlog resource,
    updating the controller and view to support this new relationship.
    
    This is a minimum set of changes as described in the Active Storage
    documentation : https://edgeguides.rubyonrails.org/active_storage_overview.html

--- app/controllers/vlogs_controller.rb
+++ app/controllers/vlogs_controller.rb
@@ -69,6 +69,6 @@ class VlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def vlog_params
-      params.require(:vlog).permit(:title, :dated_on)
+      params.require(:vlog).permit(:title, :dated_on, files: [])
     end
 end
--- app/models/vlog.rb
+++ app/models/vlog.rb
@@ -1,2 +1,3 @@
 class Vlog < ApplicationRecord
+  has_many_attached :files
 end
